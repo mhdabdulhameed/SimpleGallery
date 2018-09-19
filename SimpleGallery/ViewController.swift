8f755d829acb3908508814cf2fbee9dbd67f9a5c
Mohamed Abdul-Hameed <mhd-abdulhameed@mohameds-macbook-pro-2.local>
2018-09-19 21:45:15 +0200

    Initial Commit

---
+++ SimpleGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleGallery
+//
+//  Created by Mohamed Abdul-Hameed on 9/19/18.
+//  Copyright © 2018 Mohamed Abdul-Hameed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
